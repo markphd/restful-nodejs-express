8b83d8177c7be0a9b90e2ce4db37c646c4e4f72b
markphd <radical.mark@gmail.com>
2016-03-13 01:22:35 +0400

    refactor app routing

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ var express = require('express'),
     app = express(),
     port = 9000;
 
+var appRouter = express.Router();
+
 app.get('/', function(req, res) {
   res.send('Running server with Nodemon & Gulp')
 })
+
+appRouter.route('/path')
+  .get(function(req, res) {
+    var responseJSON = { foo: 'bar'};
+
+    res.json(responseJSON);
+  })
+
+app.use('/api', appRouter);
 
 app.listen(port, function(req, res) {
   console.log('Success!')
