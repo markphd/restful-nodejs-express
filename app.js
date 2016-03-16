60a4a34d5a864b2ec12dadb28a66a5b7713803eb
markphd <radical.mark@gmail.com>
2016-03-16 04:26:14 +0400

    added mongoose connection to db and data model

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
+// THE ESSENTIALS
 var express = require('express'),
     app = express(),
     port = 9000,
     appRouter = express.Router();
-    
+
+// CONNECTION TO DB
+var mongoose = require('mongoose'),
+	db = mongoose.connect('mongodb://localhost/bookAPI'),
+	// DATA MODEL
+	Book = require('./models/bookModel');
+
 app.use('/api', appRouter);
 
 appRouter.route('/path')
   .get(function(req, res) {
     var responseJSON = { foo: 'bar'};
     res.json(responseJSON);
+  })
+
+appRouter.route('/Books')
+  .get(function(req, res) {
+  	Book.find(function(err, books){
+  		if (err) return err
+  		res.json(books);
+  	})
   })
 
 app.get('/', function(req, res) {
